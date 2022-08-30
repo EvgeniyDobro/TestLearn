2cf22c2e33a058fec9f8b700dab1741fe65482ed
EvgenDobro <evgendobrishkin@gmail.com>
2022-08-30 09:07:01 +0400

    first lessonn

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 
-console.log("hello,word");
+const numberOfFilms = +prompt( "Сколько фильмов вы уже просмотрели?" , '0')
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const a = prompt('последний просмотренный фильм ?',''),
+      b = +prompt('на сколько его оцените ?',''),
+      c = prompt('последний просмотренный фильм ?',''),
+      d = +prompt('на сколько его оцените ?','');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log('кол-во фильмов ' + numberOfFilms);
+console.log(personalMovieDB);
+
 
